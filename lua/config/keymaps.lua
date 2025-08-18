7f4d4cc3b4691b5e7e90d99bee677144e02e8e4c
You Qin <yqin@techyauld.com>
2025-08-18 14:18:56 +0800

    add keymap to exit terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- map <esc><esc> (double press esc) to exiting terminal (keep oneshot work as before)
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { noremap = true, silent = true })

c3cfa234786b2bdcb8bc338fd97a159307cf3fab
You Qin <yqin@techyauld.com>
2025-08-23 08:32:50 +0800

    change keymap to exit terminal (double esc works bad)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,5 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- map <esc><esc> (double press esc) to exiting terminal (keep oneshot work as before)
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
+-- map ctrl+\ to exiting terminal (keep oneshot work as before)
+vim.keymap.set('t', '<C-\\>', '<C-\\><C-n>', { noremap = true, silent = true })
